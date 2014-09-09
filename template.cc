f9a986469ff7f2f3be7f81ad500754e45d627775
Yundong Yeo <yyoud88@gmail.com>
2014-09-09 11:58:52 -0400

    add template file

---
+++ template.cc
@@ -0,0 +1,62 @@
+//#include <bits/stdc++.h>
+
+
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+#define FOR(a , b , c) for(int a = (int)b; a<=(int)c; a++)
+#define SZ(v) ((int)(v).size())
+#define ALL(o) o.begin(), o.end()
+#define pii pair <int, int>
+
+int main(void) {
+  return 0;
+}
